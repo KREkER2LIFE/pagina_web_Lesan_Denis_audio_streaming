d586605eb423c6fffcab4dc4ba909cbeeb51f524
Dodogo <denislesan2022@gmail.com>
2026-09-04 10:41:38 +0300

    stergerea registrari si adaugarea posibilitatea de a crea carduri cu titluri si descriere

--- script.js
+++ script.js
@@ -1,47 +1,29 @@
-const form = document.getElementById("registerForm");
-const message = document.getElementById("message");
 
-form.addEventListener("submit", function(event) {
+const formular = document.getElementById("musicForm");
+const carduri = document.getElementById("carduri");
+
+formular.addEventListener("submit", function(event) {
+    
     event.preventDefault();
 
-    const username = document.getElementById("username").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
-    const terms = document.getElementById("terms").checked;
+    
+    const titlu = document.getElementById("titlu").value;
+    const descriere = document.getElementById("descriere").value;
 
     
-    if (username.length < 3) {
-        message.textContent = "Numele de utilizator trebuie să aibă cel puțin 3 caractere.";
-        message.style.color = "red";
-        return;
-    }
-
-   
-    if (password.length < 6) {
-        message.textContent = "Parola trebuie să aibă cel puțin 6 caractere.";
-        message.style.color = "red";
-        return;
-    }
+    const card = document.createElement("div");
+    card.classList.add("card");
 
     
-    if (password !== confirmPassword) {
-        message.textContent = "Parolele nu coincid.";
-        message.style.color = "red";
-        return;
-    }
+    card.innerHTML = `
+        <h3>${titlu}</h3>
+        <p>${descriere}</p>
+    `;
 
     
-    if (!terms) {
-        message.textContent = "Trebuie să accepți termenii și condițiile.";
-        message.style.color = "red";
-        return;
-    }
+    carduri.appendChild(card);
 
     
-    message.textContent = "Contul a fost creat cu succes!";
-    message.style.color = "#1db954";
-
-  
-    form.reset();
+    formular.reset();
 });
+
